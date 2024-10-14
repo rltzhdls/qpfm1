98766dedce1af97a70c37fe6a569018298fad009
rltzhdls <136443821+rltzhdls@users.noreply.github.com>
2024-10-14 15:42:27 +0900

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    var vds = document.getElementById('vds');
+    
+    if (vds) {
+        vds.src = "https://vod.plaync.com/LineageW/20230208/the_last_throne_bg.mp4";
+        document.getElementById('vd').load();
+    }
+});
+
